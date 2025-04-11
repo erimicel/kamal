92ab6f7d6cbf2c149b0221a70795650ae2493bce
Erim Icel <erimicel@gmail.com>
2025-04-11 23:20:32 +0100

    Add env variable check for backup commands

--- lib/kamal/commands/accessory/backup.rb
+++ lib/kamal/commands/accessory/backup.rb
@@ -4,7 +4,14 @@ module Kamal::Commands::Accessory::Backup
 
     case type
     when :mysql, :postgresql
-      db_backup(options)
+      unless backup_env_variables_set?
+        # TODO: Add port env?
+        say "Backup not supported for #{service_name} (missing env variables)", :red
+        say "Please set BACKUP_DB_USER, BACKUP_DB_PASSWORD, and BACKUP_DB_NAME", :red
+        raise
+      end
+
+      db_backup(type, options)
     when :redis
       redis_backup(options)
     else
@@ -16,25 +23,8 @@ module Kamal::Commands::Accessory::Backup
     execute_in_existing_container "rm", remote_path
   end
 
-  def detect_accessory_type
-    return nil unless image
-
-    image_name = image.downcase
-
-    if image_name.include?("mysql") || image_name.include?("mariadb")
-      :mysql
-    elsif image_name.include?("postgres")
-      :postgresql
-    elsif image_name.include?("redis")
-      :redis
-    else
-      nil
-    end
-  end
-
   private
-    def db_backup(options = {})
-      type = detect_accessory_type
+    def db_backup(type, options = {})
       timestamp = Time.now.strftime("%Y%m%d_%H%M%S")
       filename = "#{service_name}_#{type}_backup_#{timestamp}.sql"
       remote_path = "/tmp/#{filename}"
@@ -42,16 +32,17 @@ module Kamal::Commands::Accessory::Backup
       case type
       when :mysql
         execute_in_existing_container "bash", "-c",
-          "mysqldump --single-transaction -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > #{remote_path}"
+          "mysqldump --single-transaction -u $BACKUP_DB_USER -p$BACKUP_DB_PASSWORD $BACKUP_DB_NAME > #{remote_path}"
       when :postgresql
         execute_in_existing_container "bash", "-c",
-          "PGPASSWORD=$POSTGRES_PASSWORD pg_dump -U $POSTGRES_USER -d $POSTGRES_DB -F plain > #{remote_path}"
+          "PGPASSWORD=$BACKUP_DB_PASSWORD pg_dump -U $BACKUP_DB_USER -d $BACKUP_DB_NAME -F plain > #{remote_path}"
       end
 
       [ remote_path, filename ]
     end
 
     def redis_backup(options = {})
+      # TODO: Valkey?
       timestamp = Time.now.strftime("%Y%m%d_%H%M%S")
       filename = "#{service_name}_redis_backup_#{timestamp}.rdb"
       remote_path = "/tmp/#{filename}"
@@ -60,5 +51,25 @@ module Kamal::Commands::Accessory::Backup
       execute_in_existing_container "bash", "-c", "cp /data/dump.rdb #{remote_path}"
 
       [ remote_path, filename ]
+    end
+
+    def detect_accessory_type
+      return nil unless image
+
+      image_name = image.downcase
+
+      if image_name.include?("mysql") || image_name.include?("mariadb")
+        :mysql
+      elsif image_name.include?("postgres")
+        :postgresql
+      elsif image_name.include?("redis")
+        :redis
+      else
+        nil
+      end
+    end
+
+    def backup_env_variables_set?
+      %w[ BACKUP_DB_USER BACKUP_DB_PASSWORD BACKUP_DB_NAME ].all? { |var| ENV.key?(var) }
     end
 end
